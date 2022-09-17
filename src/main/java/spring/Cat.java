eab23b2dc2d5318e5bae659007a40b19c80171ae
kaspuk.p <kaspruk.p@gmail.com>
2022-09-17 15:50:26 +0300

    Depency Injection #1

--- src/main/java/spring/Cat.java
+++ src/main/java/spring/Cat.java
@@ -1,6 +1,9 @@
 package spring;
 
 public class Cat implements  Pet{
+    public Cat(){
+        System.out.println("Cat bean is created");
+    }
     @Override
     public void say (){
         System.out.println("Meow- meow");
--- src/main/java/spring/Dog.java
+++ src/main/java/spring/Dog.java
@@ -1,6 +1,10 @@
 package spring;
 
 public class Dog implements Pet {
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
+
     @Override
     public void  say (){
         System.out.println("BoW - WoW");
