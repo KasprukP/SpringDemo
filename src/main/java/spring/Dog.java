2c8509ac389484d5e83ac024df0bbf0d77846ea7
kaspuk.p <kaspruk.p@gmail.com>
2022-09-17 14:45:54 +0300

    Inversion of Control

---
+++ src/main/java/spring/Dog.java
@@ -0,0 +1,7 @@
+package spring;
+
+public class Dog {
+    public void  say (){
+        System.out.println("Bow - wow");
+    }
+}
---
+++ src/main/java/spring/Test1.java
@@ -0,0 +1,8 @@
+package spring;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.say();
+    }
+}
