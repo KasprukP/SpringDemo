d9748914858e7b14238e9df7a89d770d5ece73fd
kaspuk.p <kaspruk.p@gmail.com>
2022-09-18 12:39:37 +0300

    Dependency Injection #3 (Set)

--- src/main/java/spring/Person.java
+++ src/main/java/spring/Person.java
@@ -3,8 +3,13 @@ package spring;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+    public Person() {
         System.out.println("Person bean is created");
+
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class person Set Pet");
         this.pet = pet;
     }
 
